75989f8ebf2f673f3601664b752acaed7ad33e68
Bossman8 <jones.boston8@gmail.com>
2019-07-27 00:24:21 -0600

    finished app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,57 @@
 var series = ["Friends", "That 70's Show", "How I Met Your Mother", "Last Man on Earth"];
 
+function displayGiphyinfo() {
+    var gif = $(this).attr("data-name")
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=uFwwzNchSZ6TyeldPwQzB79HgmZKgRhU&q=" + gif + "&limit=10&offset=0&lang=en"
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response)
+        $("#gifDiv").empty()
+        for (i = 0; i < response.data.length; i++){
+        
+
+        gifImage = $("<img>")
+
+        gifImage.attr('src',response.data[i].images.fixed_width_still.url);
+        gifImage.attr('data-still',response.data[i].images.fixed_width_still.url);
+        gifImage.attr('data-animate',response.data[i].images.fixed_width.url);
+        gifImage.attr("data-state", "still");
+        gifImage.attr("class", "gifs")
+        gifImage.append("<p>Rating : "+response.data[i].rating+"</p>")
+        
+        $("#gifDiv").append(gifImage);
+        $("#gifDiv").append("<p>Rating : "+response.data[i].rating+"</p>");
+        }
+        $(".gifs").on("click", function() {
+            // debugger;
+            // The attr jQuery method allows us to get or set the value of any attribute on our HTML element
+            var state = $(this).attr("data-state");
+            // If the clicked image's state is still, update its src attribute to what its data-animate value is.
+            // Then, set the image's data-state to animate
+            // Else set src to the data-still value
+            if (state === "still") {
+                $(this).attr("src", $(this).attr("data-animate"));
+                $(this).attr("data-state", "animate");
+              } else {
+                $(this).attr("src", $(this).attr("data-still"));
+                $(this).attr("data-state", "still");
+            }
+          });
+        
+
+        
+        
+
+
+    });
+
+}
+
+
+
 function alertMovieName() {
 
     var seriesName = $(this).attr("data-name");
@@ -35,9 +87,16 @@ $("#addSeries").on("click", function (event) {
     series.push(movie);
 
     renderButtons();
-// }
+    $("#inputSeries").val("");
+
+    // }
 });
 
-$(document).on("click", ".series", alertMovieName);
+$(document).on("click", ".series", displayGiphyinfo);
 
 renderButtons();
+
+
+
+// https://api.giphy.com/v1/gifs/search?api_key=uFwwzNchSZ6TyeldPwQzB79HgmZKgRhU&q=bird&limit=25&offset=0&rating=G&lang=en
+// API key : uFwwzNchSZ6TyeldPwQzB79HgmZKgRhU
